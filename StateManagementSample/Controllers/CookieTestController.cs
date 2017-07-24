bc6d5aa21732f5adf679ba1252fcc99a2a5426ce
marwoch <marwoch@users.noreply.github.com>
2017-07-24 15:36:42 -0700

    added session

--- StateManagementSample/Controllers/CookieTestController.cs
+++ StateManagementSample/Controllers/CookieTestController.cs
@@ -17,16 +17,29 @@ namespace StateManagementSample.Controllers
         //write the cookie
         public ActionResult CreateCookie()
         {
+            //adds to reponse automatically
+            Response.Cookies["userInfo"]["userName"] = "patrick";
+            Response.Cookies["userInfo"]["lastVisit"] = DateTime.Now.ToString();
+            Response.Cookies["userInfo"].Expires = DateTime.Now.AddDays(1);
+
+            //another cookie method
             HttpCookie cookie = new HttpCookie("Test");
             cookie.Value = "snickerdoodle";
             cookie.Expires = DateTime.Now.AddYears(1);
-            Response.Cookies.Add(cookie);
+            Response.Cookies.Add(cookie); //must add cookie to save it
+
             return View();
         }
 
         //read the cookie
         public ActionResult ReadCookie()
         {
+            //reading one value from multi-value cookie
+            var userInfoUserName = Request.Cookies["userInfo"]["userName"];
+            ViewBag.Username = userInfoUserName;
+
+
+
             HttpCookie myCookie = Request.Cookies["Test"];
             if (myCookie != null)
             {
---
+++ StateManagementSample/Controllers/SessionTestController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace StateManagementSample.Controllers
+{
+    public class SessionTestController : Controller
+    {
+        public ActionResult CreateSession()
+        {
+            Session["Test"] = "Joe"; // 55.5, false... sessions store objects
+            return View();
+        }
+
+        public ActionResult ReadSession()
+        {
+            if (Session["Test"] != null)
+            {
+                string name = Session["Test"] as string;
+                ViewBag.Data = name;
+            }
+            return View();
+
+        }
+    }
+}
