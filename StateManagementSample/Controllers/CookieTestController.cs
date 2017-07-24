952e970412e728ffc77ad1bbd2ef6a817fce4e3a
marwoch <marwoch@users.noreply.github.com>
2017-07-24 14:52:22 -0700

    Add project files.

---
+++ StateManagementSample/Controllers/CookieTestController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace StateManagementSample.Controllers
+{
+    public class CookieTestController : Controller
+    {
+        // GET: CookieTest
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //write the cookie
+        public ActionResult CreateCookie()
+        {
+            HttpCookie cookie = new HttpCookie("Test");
+            cookie.Value = "snickerdoodle";
+            cookie.Expires = DateTime.Now.AddYears(1);
+            Response.Cookies.Add(cookie);
+            return View();
+        }
+
+        //read the cookie
+        public ActionResult ReadCookie()
+        {
+            HttpCookie myCookie = Request.Cookies["Test"];
+            if (myCookie != null)
+            {
+                ViewBag.CookieData = myCookie.Value;
+            }
+            else
+            {
+                ViewBag.CookieData = "No cookie present";
+            }
+            return View();
+        }
+    }
+}
